e92aaf0403fcea8aed52a539b5582129efef786f
George Papadopoulos <george.719pap@gmail.com>
2023-02-16 13:00:48 +0200

    check point for TransactionalJob

--- src/main/kotlin/com/george/transactionalAwareDemo/TransactionalAwareConcurrentMap.kt
+++ src/main/kotlin/com/george/transactionalAwareDemo/TransactionalAwareConcurrentMap.kt
@@ -69,37 +69,49 @@ interface TransactionalAction<in T> {
     val value: T.() -> Any?
 }
 
+typealias Action<T> = T.() -> Any?
+
 class ConcurrentHashMapTransactionalAction<K, V>(
     override val value: ConcurrentHashMap<K, V>.() -> Any?
 ) : TransactionalAction<ConcurrentHashMap<K, V>>
 
 open class TransactionalJob<T>(
-    private val _action: TransactionalAction<T>
+    action: TransactionalAction<T>
 ) : TransactionContext.TransactionalElement() {
     private val jobId = 0L + this.hashCode()
 
     private var state = ActionState.WAITING
-    private val actionValue = _action.value
+    private val actionValue = action.value
 
     override val key: Key<*> get() = Job
 
     val isCommitted = state == ActionState.COMMITTED
     val isROLLBACKED = state == ActionState.ROLLBACKED
 
-    private fun getActionOrNull(): TransactionalAction<T>? = if (state == ActionState.WAITING) _action else null
+    private fun getActionOrNull(): Action<T>? = if (state == ActionState.WAITING) actionValue else null
 
-    fun getActionAndCommitOrReturn(action: (action: T) -> Any?) {
+    fun getActionAndCommitOrReturn(perform: (action: Action<T>) -> Any?) {
         val canDoAction = getActionOrNull() ?: return
         try {
-            action(canDoAction)
-            state = ActionState.COMMITTED
+            perform(canDoAction)
         } catch (e: Throwable) {
-            state = ActionState.ROLLBACKED
+            tryRollback()
             throw e
         }
+        tryCommit()
     }
 
-    companion object Job : Key<TransactionalJob<*, *>>
+    private fun tryCommit() {
+        if (state != ActionState.WAITING) error("forbidden state:$state, should have been waiting")
+        state = ActionState.COMMITTED
+    }
+
+    private fun tryRollback() {
+        if (state != ActionState.WAITING) error("forbidden state:$state, should have been waiting")
+        state = ActionState.ROLLBACKED
+    }
+
+    companion object Job : Key<TransactionalJob<*>>
 
     private enum class ActionState {
         WAITING,
